2077aa190b9870358f98d5cecaa532e88ea6c6f3
Jester G. Pastor <136570458+JGPASTOR@users.noreply.github.com>
2023-08-06 17:50:03 +0800

    My Portfolio

---
+++ portfolio.js
@@ -0,0 +1,7 @@
+var toggleIcon = document.querySelector(".toggle-icon");
+
+toggleIcon.addEventListener('click', () => {
+toggleIcon.classList.toggle('#');  
+  document.body.classList.toggle("dark-mode");
+
+});
